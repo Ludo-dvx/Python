d79bf9320a9aa6e85125e6639ab15742194d94b4
Ludo <40889240+Ludo-dvx@users.noreply.github.com>
2022-12-28 18:52:11 +0100

    Update convertisseur.py
    Ajout de la convertion litres <=> m3

--- Convertisseur/convertisseur.py
+++ Convertisseur/convertisseur.py
@@ -17,6 +17,14 @@ def joule_en_watth(joule):
 # --- Convertion des watt-heures en joules ---
 def watth_en_joule(watth):
     return watth * 3600
+
+# --- Convertion des litres en metres cubes ---
+def litre_en_m3(litre):
+    return litre / 1000
+
+# --- Convertion des metres cubes en litres ---
+def m3_en_litres(m3):
+    return m3 * 1000
     
 # --- Option de convertion ---
 
@@ -27,6 +35,8 @@ def menu():
     print("2 : Radians en degrés")
     print("3 : Joules en watt-heures")
     print("4 : Watt-heures en joules")
+    print("5 : Litres en metre cube")
+    print("6 : Metres cubes en litres")
     print("q : Quitter\n")
 
     return input("Choix de convertion : ")
@@ -46,25 +56,35 @@ while continuer:
 
     if choix == "1":
         # On récupère et converti l'angle donné par l'utilisateur
-        deg = float(input("Mesure en degré : "))
+        deg = float(input("Mesure en degrés : "))
         # On affiche le résultat en convertissant en chaine de caractères
-        print("Mesure en radian : " + str(deg_en_rad(deg)))
+        print("Mesure en radians : " + str(deg_en_rad(deg)))
         # On demande si l'on souhaite faire d'autres convertions
         continuer = demande_continuer()
    
     elif choix == "2":
-        rad = float(input("Mesure en radian : "))
-        print("Mesure en degré : " + str(rad_en_deg(rad)))
+        rad = float(input("Mesure en radians : "))
+        print("Mesure en degrés : " + str(rad_en_deg(rad)))
         continuer = demande_continuer()
     
     elif choix == "3":
-        joule = float(input("Mesure en joule : "))
-        print("Mesure en watt-heure : " + str(joule_en_watth(joule)))
+        joule = float(input("Mesure en joules : "))
+        print("Mesure en watt-heures : " + str(joule_en_watth(joule)))
         continuer = demande_continuer()
 
     elif choix == "4":
-        joule = float(input("Mesure en watt-heure : "))
-        print("Mesure en joule : " + str(watth_en_joule(joule)))
+        watth = float(input("Mesure en watt heures : "))
+        print("Mesure en joules : " + str(watth_en_joule(watth)))
+        continuer = demande_continuer()
+    
+    elif choix == "5":
+        litre = float(input("Mesure en litres : "))
+        print("Mesure en metres cubes : " + str(litre_en_m3(litre)))
+        continuer = demande_continuer()
+    
+    elif choix == "6":
+        m3 = float(input("Mesure en metres cubes : "))
+        print("Mesure en litres : " + str(m3_en_litres(m3)))
         continuer = demande_continuer()
    
     elif choix == "q":
