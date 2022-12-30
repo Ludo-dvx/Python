c55a09fcfb2008f7a53d1fd4104ee609481c6ea4
Ludo <40889240+Ludo-dvx@users.noreply.github.com>
2022-12-30 17:20:26 +0100

    Update convertisseur.py
    ajout des longueurs et des masses

--- Convertisseur/convertisseur.py
+++ Convertisseur/convertisseur.py
@@ -4,6 +4,12 @@ from math import pi
 expo1 = [10**-3, 10**-2, 10**-1, 10**0, 10**1, 10**2, 10**3, 10**-3, 10**0, 10**3, 10**6, 10**9]
 unit1 = ["mL", "cL", "dL", "L", "daL", "hL", "kL", "cm3", "dm3", "m3", "dam3", "hm3", "km3"]
 
+expo2 =[10**-3, 10**-2, 10**-1, 10**0, 10**1, 10**2, 10**3, 10**5, 10**6]
+unit2 =["mg", "cg", "dg", "g", "dag", "hg", "kg", "q", "t"]
+
+expo3 = [10**-9,10**-6,10**-3,10**-2,10**-1,10**0,10**1,10**2,10**3]
+unit3 = ["nm","micrometre","mm","cm","dm","m","dam","hm","km",]
+
 #-----------Formules des convertion------------
 
 # --- Convertion des degrés en radians ---
@@ -21,8 +27,6 @@ def joule_en_watth(nbr):
 # --- Convertion des watt-heures en joules ---
 def watth_en_joule(nbr):
     return nbr * 3600
-
-
 
 
 def demande_continuer():
@@ -63,6 +67,24 @@ def energie():
     elif energie_choix == 2:
         print("Mesure en joules : " + str(watth_en_joule(nbr)))
         continuer = demande_continuer()    
+    
+def masse():
+    nbr = float(input("Quel nombre doit etre converti ?"))
+    print("liste des unites : \n0 = mg\t1 = cg\t2 = dg\n3 = g     4 = dag   5 = hg\n6 = kg\t7 = q\t 8 = t")
+    unit_d2 = int(input("Unite du nombre de depart : "))
+    unit_f2 = int(input("Unité dans lequel le nombre doit etre converti : : "))
+    convert2 = nbr * expo2[unit_d2] / expo2[unit_f2]
+    print("Le resultat est :\n%.3e"%convert2, unit2[unit_f2])
+    continuer = demande_continuer()
+
+def longueur():
+    nbr = float(input("Quel nombre doit etre converti ? "))
+    print("liste des unites : \n0 = nm\t1 = micrometre\n2 = mm\t3 = cm\t4 = dm\n5 = m     6 = dam   7 = hm\n8 = km")
+    unit_d3 = int(input("Unite du nombre de depart : "))
+    unit_f3 = int(input("Unité dans lequel le nombre doit etre converti : "))
+    convert3 = nbr * expo3[unit_d3] / expo3[unit_f3]
+    print("Le resultat est :\n%.3e"%convert3, unit3[unit_f3])
+    continuer = demande_continuer()
 
 def menu():
     # Affiche le menu et renvoie le choix de l'utilisateur
@@ -70,6 +92,8 @@ def menu():
     print("1 : Volume")
     print("2 : Angle")
     print("3 : Energie")
+    print("4 : Masse")
+    print("5 : Longueur")
     choix = float(input("Choix de la grandeur : " ))
 
     if choix == 1:
@@ -78,6 +102,11 @@ def menu():
         angle()
     elif choix == 3:
         energie()
+    elif choix == 4:
+        masse()
+    elif choix == 5:
+        longueur()
     else:
         print("Option inconnue")
 menu()
+
